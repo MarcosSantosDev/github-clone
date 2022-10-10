8eebb728506db9433496ac39a392d3a7d154e095
MarcosSantosDev <marcosadriano034@gmail.com>
2022-10-09 21:48:37 -0300

    fix: storybook dynamic import

--- .storybook/main.cjs
+++ .storybook/main.cjs
@@ -1,18 +1,25 @@
+const tsconfigPaths = require("vite-tsconfig-paths");
+
 module.exports = {
-  "stories": [
-    "../src/**/*.stories.mdx",
-    "../src/**/*.stories.@(js|jsx|ts|tsx)"
+  stories: [
+    "../src/common/components/**/*.stories.mdx",
+    "../src/common/components/**/*.stories.@(js|jsx|ts|tsx)"
   ],
-  "addons": [
+  addons: [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
     "@storybook/addon-interactions"
   ],
-  "framework": "@storybook/react",
-  "core": {
+  framework: "@storybook/react",
+  core: {
     "builder": "@storybook/builder-vite"
   },
-  "features": {
+  features: {
     "storyStoreV7": true
+  },
+  viteFinal: async (config) => {
+    config.plugins.push(tsconfigPaths.default());
+
+    return config;
   }
 }
