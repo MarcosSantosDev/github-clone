c2d14892c5444d4297c07e9c90fd46fc2ced8eb2
MarcosSantosDev <marcosadriano034@gmail.com>
2023-01-26 00:10:41 -0300

    feat: Repositories
    - create repository search bar;
    - list mock repositories;

--- src/common/components/context/ProfileOverview/NavigationTabs/TabContent/Repositories/Repositories.tsx
+++ src/common/components/context/ProfileOverview/NavigationTabs/TabContent/Repositories/Repositories.tsx
@@ -1,9 +1,34 @@
+import { Input } from '@/common/components/form';
+import { Button, RepositoryOverview } from '@/common/components/structure';
+
+import { repositoriesMocked } from './mock';
 import * as S from './Repositories.styles';
 
 const Repositories = () => {
   return (
     <S.ContainerDiv>
-      <h2>Repositories</h2>
+      <S.RepositorySearchWrapperDiv>
+        <Input type="text" placeholder="Find a repository..." />
+        <S.RepositorySearchButtonGroupDiv>
+          <Button type="button" size="medium">
+            Type
+          </Button>
+          <Button type="button" size="medium">
+            Language
+          </Button>
+          <Button type="button" size="medium">
+            Sort
+          </Button>
+          <Button type="button" size="medium">
+            New
+          </Button>
+        </S.RepositorySearchButtonGroupDiv>
+      </S.RepositorySearchWrapperDiv>
+      <S.RepositoryListWrapperDiv>
+        {repositoriesMocked.map(repository => (
+          <RepositoryOverview key={repository.id} repository={repository} />
+        ))}
+      </S.RepositoryListWrapperDiv>
     </S.ContainerDiv>
   );
 };
