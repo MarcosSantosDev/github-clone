66083300d209485a7e61bfa44c20855df708140b
MarcosSantosDev <marcosadriano034@gmail.com>
2023-02-09 15:44:00 -0300

    refactor: LinkWrapper

--- src/common/components/structure/LinkWrapper/LinkWrapper.tsx
+++ src/common/components/structure/LinkWrapper/LinkWrapper.tsx
@@ -1,30 +1,3 @@
-import { LinkProps as LinkRRDProps } from 'react-router-dom';
-
 import * as S from './LinkWrapper.styles';
 
-type CustomProps = {
-  disableHoverUnderline?: boolean;
-};
-
-type LinkWrapperProps = LinkRRDProps & CustomProps;
-
-function getLinkCustomStyles({
-  disableHoverUnderline = false,
-}: Pick<CustomProps, 'disableHoverUnderline'>): React.CSSProperties {
-  const staticStyles = {
-    textDecoration: disableHoverUnderline ? 'none' : '',
-  };
-
-  return staticStyles;
-}
-
-export const LinkWrapper = ({
-  disableHoverUnderline,
-  ...props
-}: LinkWrapperProps) => {
-  const linkCustomStyles = getLinkCustomStyles({
-    disableHoverUnderline,
-  });
-
-  return <S.LinkWrapper {...props} style={linkCustomStyles} />;
-};
+export const LinkWrapper = S.LinkWrapper;
